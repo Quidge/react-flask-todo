79b227edba359bc780cf943bf0cc849ed0114e53
Jonathan Demirgian <lirum@Jonathans-Air.attlocal.net>
2019-08-14 19:58:49 -0400

    Remove task creation button; restyle for loss of button

--- client/app/src/Header.js
+++ client/app/src/Header.js
@@ -13,43 +13,24 @@ const StyledHeader = styled.div`
   form {
     display: flex;
     align-items: center;
+    justify-content: center;
     min-height: 2.5em;
-    margin: .3em .5em;
     width: 100%
     border-radius: 7px;
     border: none;
     box-sizing: content-box;
-    font-size: 1.5em;
-
-    input,
-    button {
-      // margin: 0 1em;
-      font-size: 20px
-    }
+    font-size: 16px;
 
     input[type=text] {
-      flex: 0 1 100%;
+      font-size: 1.5em;
+      flex: 0 1 75%;
       border: none;
       border-bottom: 3px solid #dfe1e6;
-      margin-left: 1em;
-      font-size: 16px;
       transition: border-bottom 0.30s ease-in-out;
+      margin: .5em auto;
     }
     input[type=text]:focus {
       border-bottom: 3px solid #BADCAB;
-    }
-
-    button {
-      background-color: white;
-      color: #090F17;
-      border: none;
-      cursor: pointer;
-      margin-right: 1em;
-      margin-left: .5em;
-
-      &:disabled {
-        color: lightgrey;
-      }
     }
   }
 `
@@ -64,13 +45,6 @@ const Header = (props) => {
           value={props.taskTitle}
           onChange={props.handleChange}
           autocomplete={false}/>
-        <button
-          type="submit"
-          name="submit"
-          value="Create Task"
-          disabled={props.disabled}>
-          <i className="fas fa-plus"/>
-        </button>
       </form>
     </StyledHeader>
   )
