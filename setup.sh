6d2ab55f07be4adcee09d23e12d5b577f1cdb607
Jonathan Demirgian <lirum@Jonathans-Air.attlocal.net>
2019-08-14 20:26:12 -0400

    setup modifications

--- setup.sh
+++ setup.sh
@@ -1,14 +1,16 @@
 # Seed and setup backend
 
 # Create venv
+echo "Creating venv"
 cd server
 python3 -m venv venv
 source venv/bin/activate
 
-# install backend
-pip install requirements.txt
+# install backend modules
+echo "Installing python modules"
+pip install -r requirements.txt
 
-# seed app with 5 generated tasks and start the server
+# seed app with 5 generated tasks
 export FLASK_APP=application
 
 flask fill-db 5
@@ -17,3 +19,5 @@ deactivate
 
 cd ../client/app
 npm install
+
+cd ../../
